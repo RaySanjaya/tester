5d88aa997bd6366e6972536b4759d92ba03d61e6
Ray Sanjaya <raysanjayagulo@gmail.com>
2024-05-02 17:59:29 +0700

    first commit

---
+++ src/app/components/Content.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useState } from "react"
+
+export const Content = () => {
+  const [value, setValue] = useState<number>(1)
+
+  const tambah = () => {
+    setValue(value + 1)
+  }
+
+  const kurang = () => {
+    setValue(value - 1)
+  }
+
+  return (
+    <>
+      <h1>{ value }</h1>  
+      <div className="flex flex-col">
+        <button onClick={ () => { tambah() } }>Tambah</button>
+        <button onClick={ () => { kurang() }}>kurang</button>
+      </div>
+    </>
+  )
+}
